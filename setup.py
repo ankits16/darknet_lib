324a48b6544d225433882bab36207e61df50849a
Ankit Sachan <>
2021-12-16 14:49:17 +0530

    no message

--- setup.py
+++ setup.py
@@ -3,6 +3,10 @@ from setuptools.command.build_ext import build_ext
 from setuptools.command.build_py import build_py
 from setuptools import setup, find_packages
 import os
+import subprocess
+import sys
+
+
 class Build(build_ext):
  """Customized setuptools build command - builds protos on build."""
  def run(self):
@@ -10,8 +14,8 @@ class Build(build_ext):
      protoc_command = ["echo **************** ankit"]
      os.chdir('./src/ai_darknet_reshaped')
      os.system('echo **************** ankit')
-     # if subprocess.call(protoc_command) != 0:
-     #     sys.exit(-1)
+     if subprocess.call(protoc_command) != 0:
+         sys.exit(-1)
      os.chdir('.')
 
      build_ext.run(self)
@@ -22,5 +26,7 @@ setup(
  version='1.0',
  description='Darknet lib for inference',
  packages=find_packages(),
-
+ cmdclass={
+     'build_ext': Build,
+ },
 )
