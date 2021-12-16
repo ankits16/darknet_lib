cb1956f3363c95760e37b6e3ec8bc9fae3d9153f
Ankit Sachan <>
2021-12-16 17:52:47 +0530

    no message

--- setup.py
+++ setup.py
@@ -18,8 +18,8 @@ class Build(build_ext):
      root_dir = os.path.dirname(os.path.realpath(__file__))
      print(f'<<<<<<<<<<<<<<<<< build dir {build_dir}')
      print(f'<<<<<<<<<<<<<<<<< start root_dir dir {root_dir}')
-     # os.path.join(root_dir, 'darknet_alex')
-     for file in os.listdir(root_dir):
+     # os.path.join(root_dir, 'ai_darknet_lib', 'darknet_alex')
+     for file in os.listdir(os.path.join(root_dir, 'ai_darknet_lib', 'darknet_alex')):
          print(f'<<<<<<<<<<<<<<<<<<<<<<<< {file}')
      print(f'<<<<<<<<<<<<<<<<< end root_dir dir {root_dir}')
      print(f'************************ cwd  before make {os.getcwd()}')
